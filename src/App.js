74c253bea62e08c65a13a14ecca2815deabc39cd
niraj-bit <nirajmishra0311@gmail.com>
2021-08-13 08:48:02 +0530

    re

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Internship } from './Components/Internship';
+import BootstrapTable from './Components/BootstrapTable';
+// import DataTables from './Components/DataTables';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Internship/>
+      {/* <BootstrapTable/> */}
+      {/* <DataTables/> */}
     </div>
   );
 }
